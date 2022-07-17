81ba780407b582ae3120fa9bcb2245f0a6e82284
indeokiya <109320408+indeokiya@users.noreply.github.com>
2022-07-18 02:27:11 +0900

    Time: 38 ms (85.59%), Space: 13.8 MB (63.60%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -4,16 +4,13 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         cnt0, cnt2 = 0, -1
-        tmp = None
         i = 0
         while i < len(nums):
             if nums[i] == 0:
                 if i == cnt0:
                     i += 1
                 else:
-                    tmp = nums[cnt0]
-                    nums[cnt0] = 0
-                    nums[i] = tmp
+                    nums[cnt0], nums[i] = 0, nums[cnt0]
                 cnt0 += 1
                 
             elif nums[i] == 1:
@@ -23,8 +20,6 @@ class Solution:
                 if i - cnt2 > len(nums):
                     break
                 else:
-                    tmp = nums[cnt2]
-                    nums[cnt2] = 2
-                    nums[i] = tmp
+                    nums[cnt2], nums[i] = 2, nums[cnt2]
                     cnt2 -= 1
             
