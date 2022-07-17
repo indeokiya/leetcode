429a3cac044ab274b5ac401e43590f289b982e02
indeokiya <109320408+indeokiya@users.noreply.github.com>
2022-07-17 15:27:20 +0900

    Time: 5337 ms (13.36%), Space: 18.1 MB (72.14%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -6,6 +6,8 @@ class Solution:
         for i in range(len(nums)-2):
             if nums[-1] + nums[-2] + nums[i] < 0:
                 continue
+            if nums[i] + nums[i+1] + nums[i+2] > 0:
+                break
             low, high = i+1, len(nums)-1
             while low < high:
                 cur_2sum = nums[low] + nums[high]
