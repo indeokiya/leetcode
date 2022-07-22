d994463290e089b0afd1a2d760b61134fb8483c8
indeokiya <109320408+indeokiya@users.noreply.github.com>
2022-07-22 09:23:54 +0900

    Time: 719 ms (5.00%), Space: 13.9 MB (60.71%) - LeetHub

--- 415-add-strings/415-add-strings.py
+++ 415-add-strings/415-add-strings.py
@@ -1,3 +1,21 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
-        return str(int(num1) + int(num2))
+        #ascii code: ord('1') == 49
+        ans = ""
+        intSum = self.convertIntoNum(num1) + self.convertIntoNum(num2)
+        print(intSum)
+        if intSum == 0:
+            return "0"
+        while intSum > 0 :
+            ans = chr(intSum%10+ord('0')) + ans
+            intSum = intSum//10
+        return ans
+           
+    def convertIntoNum(self, num: str) -> int:
+        ans = 0
+        len1 = len(num)
+        while len1 > 0:
+            ans += (ord(num[-len1]) - ord('0')) * (10**(len1-1))
+            len1 -= 1
+        return ans
+            
