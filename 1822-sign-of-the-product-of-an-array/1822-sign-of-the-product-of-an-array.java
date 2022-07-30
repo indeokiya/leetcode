c3690af012b5a135c461ddc197b92fb42485aa1c
indeokiya <109320408+indeokiya@users.noreply.github.com>
2022-07-30 17:54:11 +0900

    Time: 1 ms (75.66%), Space: 44.8 MB (7.11%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
@@ -3,7 +3,7 @@ class Solution {
         int ans = 1;
         for (int num: nums){
             if (num==0) return 0;
-            ans *= (num > 0) ? 1 : -1;
+            if (num<0) ans *= -1;
         }
         return ans;
     }
