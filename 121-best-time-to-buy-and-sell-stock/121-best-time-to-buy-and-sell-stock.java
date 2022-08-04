cc58b763993d347a25e3b7a9cb955add2553f2c2
indeokiya <109320408+indeokiya@users.noreply.github.com>
2022-08-04 22:41:26 +0900

    Time: 2 ms (93.63%), Space: 58.9 MB (95.49%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -3,11 +3,22 @@ class Solution {
         int local_min = prices[0];
         int local_profit = 0;
         
-        for (int price: prices) {
-            if (price < local_min) {
-                local_min = price;
-            } else if (price > local_min) {
-                int tmp_profit = price - local_min;
+        // for (int price: prices) {
+        //     if (price < local_min) {
+        //         local_min = price;
+        //     } else if (price > local_min) {
+        //         int tmp_profit = price - local_min;
+        //         if (tmp_profit > local_profit) {
+        //             local_profit = tmp_profit;
+        //         }
+        //     }
+        // }
+        
+        for (int i=0; i<prices.length; i++) {
+            if (prices[i] < local_min) {
+                local_min = prices[i];
+            } else if (prices[i] > local_min) {
+                int tmp_profit = prices[i] - local_min;
                 if (tmp_profit > local_profit) {
                     local_profit = tmp_profit;
                 }
