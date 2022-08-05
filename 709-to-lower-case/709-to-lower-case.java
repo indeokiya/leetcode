551f56af28de0a1544c19c67efa006f5a5a61a08
indeokiya <109320408+indeokiya@users.noreply.github.com>
2022-08-05 23:22:55 +0900

    Time: 1 ms (79.59%), Space: 42.4 MB (33.35%) - LeetHub

---
+++ 709-to-lower-case/709-to-lower-case.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String toLowerCase(String s) {
+        // StringBuilder sb = new StringBuilder;
+        // int dif = 
+        // for (int i=0; i<s.length(); i++) {
+        //     sb.append((char)((int))
+        // }
+        // return sb.toString();
+        return s.toLowerCase();
+    }
+}
