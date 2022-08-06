16a82198413f07c17337ae813f3612230644e0a5
indeokiya <109320408+indeokiya@users.noreply.github.com>
2022-08-06 20:34:57 +0900

    Time: 2 ms (74.13%), Space: 73 MB (79.24%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -3,17 +3,8 @@ class Solution {
         //
         int previous_sum = Integer.MIN_VALUE;
         int candidate = 0;
-        // for (int num: nums) {
-        //     candidate += num;
-        //     if (candidate > previous_sum) {
-        //         previous_sum = candidate;
-        //     }
-        //     if (candidate < 0) {
-        //         candidate = 0;
-        //     }
-        // }
-        for (int i=0; i<nums.length; i++) {
-            candidate += nums[i];
+        for (int num: nums) {
+            candidate += num;
             if (candidate > previous_sum) {
                 previous_sum = candidate;
             }
@@ -21,6 +12,15 @@ class Solution {
                 candidate = 0;
             }
         }
+        // for (int i=0; i<nums.length; i++) {
+        //     candidate += nums[i];
+        //     if (candidate > previous_sum) {
+        //         previous_sum = candidate;
+        //     }
+        //     if (candidate < 0) {
+        //         candidate = 0;
+        //     }
+        // }
         return previous_sum;
     }
 }
