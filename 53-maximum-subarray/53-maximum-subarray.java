e7a734881516b90e23977fcd3fa9d78e0f2aa115
indeokiya <109320408+indeokiya@users.noreply.github.com>
2022-08-06 20:34:35 +0900

    Time: 1 ms (100.00%), Space: 81.6 MB (14.20%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -3,15 +3,23 @@ class Solution {
         //
         int previous_sum = Integer.MIN_VALUE;
         int candidate = 0;
-        for (int num: nums) {
-            candidate += num;
+        // for (int num: nums) {
+        //     candidate += num;
+        //     if (candidate > previous_sum) {
+        //         previous_sum = candidate;
+        //     }
+        //     if (candidate < 0) {
+        //         candidate = 0;
+        //     }
+        // }
+        for (int i=0; i<nums.length; i++) {
+            candidate += nums[i];
             if (candidate > previous_sum) {
                 previous_sum = candidate;
             }
             if (candidate < 0) {
                 candidate = 0;
             }
-
         }
         return previous_sum;
     }
