ca547a7419140f2377a56a1e7f73fd68dd24ad2c
indeokiya <109320408+indeokiya@users.noreply.github.com>
2022-08-08 16:12:09 +0900

    Time: 10 ms (77.90%), Space: 67.6 MB (83.11%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Set<Integer> set = new HashSet<>(nums.length);
+        Set<Integer> set = new HashSet<>();
         for (int num: nums){
             if (set.contains(num)) {
                 return true;
@@ -9,5 +9,8 @@ class Solution {
             }
         }
         return false;
+        
+        
+        
     }
 }
