27bd09a7bad9024f0c04f6da4c61b2e446044e76
indeokiya <109320408+indeokiya@users.noreply.github.com>
2022-08-08 16:07:12 +0900

    Time: 6 ms (60.23%), Space: 43.6 MB (62.79%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        int len = s.length();
+        int xor1 = 0;
+        int xor2 = 0;
+        
+        for (int i=0; i<len; i++) {
+            xor1 ^= s.charAt(i);
+            xor2 ^= t.charAt(i);
+        }
+        
+        if (xor1 != xor2) return false;
+        
+        int[] arr = new int[26];
+        for (int i=0; i<len; i++) {
+            arr[s.charAt(i)-'a']++;
+            arr[t.charAt(i)-'a']--;
+        }
+        for (int i=0; i<26; i++) {
+            if (arr[i] != 0) return false; 
+        }
+        return true;
+    }
+}
