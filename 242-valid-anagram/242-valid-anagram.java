e2fc93cf8931e540913257bd8aa590f35361e92a
indeokiya <109320408+indeokiya@users.noreply.github.com>
2022-08-08 16:07:36 +0900

    Time: 4 ms (84.45%), Space: 43.3 MB (79.85%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,18 +1,19 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) return false;
-        int len = s.length();
-        int xor1 = 0;
-        int xor2 = 0;
+//         int len = s.length();
+//         int xor1 = 0;
+//         int xor2 = 0;
         
-        for (int i=0; i<len; i++) {
-            xor1 ^= s.charAt(i);
-            xor2 ^= t.charAt(i);
-        }
+//         for (int i=0; i<len; i++) {
+//             xor1 ^= s.charAt(i);
+//             xor2 ^= t.charAt(i);
+//         }
         
-        if (xor1 != xor2) return false;
+//         if (xor1 != xor2) return false;
         
         int[] arr = new int[26];
+        int len = s.length();
         for (int i=0; i<len; i++) {
             arr[s.charAt(i)-'a']++;
             arr[t.charAt(i)-'a']--;
