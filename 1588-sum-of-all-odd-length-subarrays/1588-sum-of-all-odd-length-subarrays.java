43006afb5401cf9b258c5faf3980d8b3d84d2c3b
indeokiya <109320408+indeokiya@users.noreply.github.com>
2022-08-08 17:31:10 +0900

    Time: 1 ms (76.32%), Space: 41.7 MB (58.63%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.java
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.java
@@ -11,22 +11,23 @@ class Solution {
         //     }
         // }
         
-        // for (int i=0; i<len; i++) {
-        //     ans += arr[i]*((((arr.length-i)*(i+1))+1)/2);
-        // }
-        
-        for (int i = 0; i < len; i++) {
-            for (int j = i + 1; j <= len; j += 2) {
-			// iterate over sub-array
-                for (int k = i; k < j; k++) {
-                    ans += arr[k];
-            }
+        for (int i=0; i<len; i++) {
+            int count = (len-i)*(i+1); // all count
+            count = (count+1)/2;
+            ans += arr[i]*count;
         }
-    }
+        // ???
+        
+
         return ans;
     }
 }
-
+// 11*1
+// 10*2
+// 9*3
+// 8*4
+// 2*10
+// 1*11
 
 // 1 1 1 1 1  1 1 1 1 1  1  1
 // 1 2 3 3 3  3 3 3 3 2  1  3
