510cec30f48d16ae9e9979fdda082f15fff671e7
indeokiya <109320408+indeokiya@users.noreply.github.com>
2022-08-10 23:53:47 +0900

    Time: 0 ms (100.00%), Space: 42 MB (56.63%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.java
+++ 54-spiral-matrix/54-spiral-matrix.java
@@ -8,29 +8,25 @@ class Solution {
         int num = 0;
         
         while (true) {
-            if (left_bound == right_bound && upper_bound == lower_bound) {
-                ans.add(matrix[upper_bound][upper_bound]);
-                if (++num == m*n) return ans;
-            }
-            for (int j=left_bound; j<right_bound; j++) {
+            for (int j=left_bound; j<=right_bound; j++) {
                 ans.add(matrix[upper_bound][j]);
                 // System.out.println(ans.size());
                 if (++num == m*n) return ans;
             }
 
-            for (int i=upper_bound; i<lower_bound; i++) {
+            for (int i=upper_bound+1; i<lower_bound; i++) {
                 ans.add(matrix[i][right_bound]);
                                 // System.out.println(ans.size());
                 if (++num == m*n) return ans;
             }
             
-            for (int j=right_bound; j>left_bound; j--) {
+            for (int j=right_bound; j>=left_bound; j--) {
                 ans.add(matrix[lower_bound][j]);
                                 // System.out.println(ans.size());
                 if (++num == m*n) return ans;   
             }
             
-            for (int i=lower_bound; i>upper_bound; i--) {
+            for (int i=lower_bound-1; i>upper_bound; i--) {
                 ans.add(matrix[i][left_bound]);
                                 // System.out.println(ans.size());
                 if (++num == m*n) return ans;   
