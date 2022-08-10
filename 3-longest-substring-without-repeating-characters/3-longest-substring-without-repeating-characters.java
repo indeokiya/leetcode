73811fabeef9e3ce7a3c13d3aa655cd79069f549
indeokiya <109320408+indeokiya@users.noreply.github.com>
2022-08-10 22:55:00 +0900

    Time: 11 ms (65.69%), Space: 44.2 MB (73.47%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -4,26 +4,37 @@ class Solution {
         int left = 0;
         int right = 0; 
         
-        HashSet<Character> mySet = new HashSet<>();
+        // HashSet<Character> mySet = new HashSet<>();
+        // while (right < s.length()) {
+        //     if (mySet.contains(s.charAt(right))) {
+        //         ans = Math.max(ans, mySet.size());
+        //         while(true) {
+        //             // mySet.remove(s.charAt(left));
+        //             if (s.charAt(left) != s.charAt(right)) {
+        //                 mySet.remove(s.charAt(left++));
+        //             } else {
+        //                 left++;
+        //                 right++;
+        //                 break;
+        //             }
+        //         }
+        //     } else {
+        //         mySet.add(s.charAt(right++));
+        //     }
+        // }
+        // ans = Math.max(ans, mySet.size());
+        // Time complexity: O(2n)
+
+        HashMap<Character, Integer> myMap = new HashMap<>();
         while (right < s.length()) {
-            if (mySet.contains(s.charAt(right))) {
-                ans = Math.max(ans, mySet.size());
-                while(true) {
-                    // mySet.remove(s.charAt(left));
-                    if (s.charAt(left) != s.charAt(right)) {
-                        mySet.remove(s.charAt(left++));
-                    } else {
-                        left++;
-                        right++;
-                        break;
-                    }
-                }
-            } else {
-                mySet.add(s.charAt(right++));
-            }
+            char tmp = s.charAt(right);
+            if (myMap.containsKey(tmp)) {
+                left = Math.max(myMap.get(tmp)+1, left);
+            }   
+                
+            myMap.put(tmp, right++);
+            ans = Math.max(ans, right-left);
         }
-        ans = Math.max(ans, mySet.size());
-        
         
         return ans;
     }
