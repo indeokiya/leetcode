e3e191d7f2eeb61c7e7d991a170960b42f662fe7
indeokiya <109320408+indeokiya@users.noreply.github.com>
2022-08-16 14:58:49 +0900

    Time: 24 ms (70.87%), Space: 64.2 MB (57.81%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
@@ -1,30 +1,27 @@
 class Solution {
     private int[][] distance;
     private int[][] arr;
-    private boolean[][] visited;
     private Queue<Integer> q;
     
     public void bfs(int i, int j) {
-        arr[i][j] = 1;
-
-        if (i>0 && arr[i-1][j] == 0 && visited[i-1][j] == false) {
+        if (i>0 && arr[i-1][j] == 0) {
             q.add((i-1)*arr.length+j);
-            visited[i-1][j] = true;
+            arr[i-1][j] = 1;
             distance[i-1][j] = distance[i][j] + 1;
         }
-        if (i<arr.length-1 && arr[i+1][j] == 0 && visited[i+1][j] == false) {
+        if (i<arr.length-1 && arr[i+1][j] == 0) {
             q.add((i+1)*arr.length+j);
-            visited[i+1][j] = true;
+            arr[i+1][j] = 1;
             distance[i+1][j] = distance[i][j] + 1;
         }
-        if (j>0 && arr[i][j-1] == 0 && visited[i][j-1] == false) {
+        if (j>0 && arr[i][j-1] == 0) {
             q.add(i*arr.length+j-1);
-            visited[i][j-1] = true;
+            arr[i][j-1] = 1;
             distance[i][j-1] = distance[i][j] + 1;
         }
-        if (j<arr.length-1 && arr[i][j+1] == 0 && visited[i][j+1] == false) {
+        if (j<arr.length-1 && arr[i][j+1] == 0) {
             q.add(i*arr.length+j+1);
-            visited[i][j+1] = true;
+            arr[i][j+1] = 1;
             distance[i][j+1] = distance[i][j] + 1;
         }
     }
@@ -32,14 +29,12 @@ class Solution {
     public int maxDistance(int[][] grid) {
         distance = new int[grid.length][grid.length];
         arr = grid;
-        visited = new boolean[grid.length][grid.length];
         q = new LinkedList<>();
         
         for (int i=0; i<arr.length; i++) {
             for (int j=0; j<arr.length; j++) {
                 if (arr[i][j] == 1) {
                     q.add(i*arr.length+j);
-                    visited[i][j] = true;
                 }
             }
         }
