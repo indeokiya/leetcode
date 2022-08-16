003719d91568bc2c5686489b4ef4c9d9d6e65457
indeokiya <109320408+indeokiya@users.noreply.github.com>
2022-08-16 15:17:15 +0900

    Time: 20 ms (82.33%), Space: 65.3 MB (30.95%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
@@ -1,40 +1,32 @@
 class Solution {
-    private int[][] distance;
-    private int[][] arr;
-    private Queue<Integer> q;
+    private Queue<int[]> q;
     
-    public void bfs(int i, int j) {
+    public void bfs(int[][] arr, int i, int j) {
         if (i>0 && arr[i-1][j] == 0) {
-            q.add((i-1)*arr.length+j);
+            q.add(new int[]{i-1, j});
             arr[i-1][j] = 1;
-            distance[i-1][j] = distance[i][j] + 1;
         }
         if (i<arr.length-1 && arr[i+1][j] == 0) {
-            q.add((i+1)*arr.length+j);
+            q.add(new int[]{i+1, j});
             arr[i+1][j] = 1;
-            distance[i+1][j] = distance[i][j] + 1;
         }
         if (j>0 && arr[i][j-1] == 0) {
-            q.add(i*arr.length+j-1);
+            q.add(new int[]{i, j-1});
             arr[i][j-1] = 1;
-            distance[i][j-1] = distance[i][j] + 1;
         }
         if (j<arr.length-1 && arr[i][j+1] == 0) {
-            q.add(i*arr.length+j+1);
+            q.add(new int[]{i, j+1});
             arr[i][j+1] = 1;
-            distance[i][j+1] = distance[i][j] + 1;
         }
     }
     
     public int maxDistance(int[][] grid) {
-        distance = new int[grid.length][grid.length];
-        arr = grid;
         q = new LinkedList<>();
         
-        for (int i=0; i<arr.length; i++) {
-            for (int j=0; j<arr.length; j++) {
-                if (arr[i][j] == 1) {
-                    q.add(i*arr.length+j);
+        for (int i=0; i<grid.length; i++) {
+            for (int j=0; j<grid.length; j++) {
+                if (grid[i][j] == 1) {
+                    q.add(new int[]{i, j});
                 }
             }
         }
@@ -43,17 +35,16 @@ class Solution {
             return -1;
         }
         
+        int ans = -1;
+        int size;
         while(!q.isEmpty()) {
-            bfs(q.peek()/arr.length, q.poll()%arr.length);
-        }
-        
-        int ans = 0;
-        for (int i=0; i<arr.length; i++) {
-            for (int j=0; j<arr.length; j++) {
-                if (distance[i][j] > ans) ans = distance[i][j];
+            size = q.size();
+            for (int i=0; i<size; i++) {
+                bfs(grid, q.peek()[0], q.poll()[1]);               
             }
+            ans++;
         }
         
-        return ans;
+        return ans > 0 ? ans : 0;
     }
 }
