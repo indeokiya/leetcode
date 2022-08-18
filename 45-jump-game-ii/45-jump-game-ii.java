cf1e55ff7788b6f4e307234ed1c2ea330b8130a2
indeokiya <109320408+indeokiya@users.noreply.github.com>
2022-08-18 21:46:56 +0900

    Time: 3 ms (55.34%), Space: 50.1 MB (14.91%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -2,23 +2,40 @@ class Solution {
     private int[] memo;
     
     public int jump(int[] nums) {
-        memo = new int[nums.length];
-        for (int i=nums.length-2; i>=0; i--) {
-            if (i+nums[i] >= nums.length-1) {
-                memo[i] = 1;
-            } else {
-                int limit = Math.min(i+nums[i], nums.length-2);
-                memo[i] = Integer.MAX_VALUE;
-                for (int j=i+1; j<=limit; j++) {
-                    if (memo[j] < 0) continue;
-                    memo[i] = Math.min(memo[i], memo[j]);
-                }
-                memo[i]++;
+//  O(n^2) dp
+//         memo = new int[nums.length];
+//         for (int i=nums.length-2; i>=0; i--) {
+//             if (i+nums[i] >= nums.length-1) {
+//                 memo[i] = 1;
+//             } else {
+//                 int limit = Math.min(i+nums[i], nums.length-2);
+//                 memo[i] = Integer.MAX_VALUE;
+//                 for (int j=i+1; j<=limit; j++) {
+//                     if (memo[j] < 0) continue;
+//                     memo[i] = Math.min(memo[i], memo[j]);
+//                 }
+//                 memo[i]++;
+//             }
+//         }
+        
+//         System.out.println(Arrays.toString(memo));
+        
+//         return memo[0];
+        
+// greedy
+        
+        int farthest = 0, currentReach = 0; 
+        int count = 0;
+        
+        for (int i=0; i<nums.length-1; i++) {
+            farthest = Math.max(farthest, i+nums[i]);
+            
+            if (i == currentReach) {
+                count++;
+                currentReach = farthest;
             }
         }
         
-        System.out.println(Arrays.toString(memo));
-        
-        return memo[0];
+        return count;
     }
 }
