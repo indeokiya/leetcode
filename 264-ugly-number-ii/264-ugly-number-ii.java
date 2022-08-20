c14fcd4237383183f6c454aa7aea193a80e4a77c
indeokiya <109320408+indeokiya@users.noreply.github.com>
2022-08-20 21:52:58 +0900

    Time: 4 ms (83.51%), Space: 42.5 MB (44.09%) - LeetHub

--- 264-ugly-number-ii/264-ugly-number-ii.java
+++ 264-ugly-number-ii/264-ugly-number-ii.java
@@ -7,33 +7,11 @@ class Solution {
         int multiplied3 = arr[idx3] * 3;
         int multiplied5 = arr[idx5] * 5;
         
-        if (multiplied2 <= arr[i-1]) {
-            idx2++;
-            dp(i);
-            return;
-        }
-        if (multiplied3 <= arr[i-1]) {
-            idx3++;
-            dp(i);
-            return;
-        }
-        if (multiplied5 <= arr[i-1]) {
-            idx5++;
-            dp(i);
-            return;
-        }
+        arr[i] = Math.min(multiplied2, Math.min(multiplied3, multiplied5));
         
-        
-        if (multiplied2 < multiplied3 && multiplied2 < multiplied5) {
-            arr[i] = multiplied2;
-            idx2++;
-        } else if (multiplied3 < multiplied5) {
-            arr[i] = multiplied3;
-            idx3++;
-        } else {
-            arr[i] = multiplied5;
-            idx5++;
-        }
+        if (arr[i] == multiplied2) idx2++;
+        if (arr[i] == multiplied3) idx3++;
+        if (arr[i] == multiplied5) idx5++;
     }
     
     public int nthUglyNumber(int n) {
@@ -42,8 +20,7 @@ class Solution {
         for (int i=1; i<n; i++) {
             dp(i);
         }
-        
-        System.out.println(Arrays.toString(arr));
+
         return arr[n-1];
     }
 }
