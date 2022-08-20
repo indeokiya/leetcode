27b1fb8bfc93156d2473a3f9862e7664890480d9
indeokiya <109320408+indeokiya@users.noreply.github.com>
2022-08-21 00:52:59 +0900

    Time: 2 ms (86.82%), Space: 235.2 MB (45.02%) - LeetHub

---
+++ 1490-clone-n-ary-tree/1490-clone-n-ary-tree.java
@@ -0,0 +1,33 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    
+    public Node() {
+        children = new ArrayList<Node>();
+    }
+    
+    public Node(int _val) {
+        val = _val;
+        children = new ArrayList<Node>();
+    }
+    
+    public Node(int _val,ArrayList<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public Node cloneTree(Node root) {
+        if (root == null) return null;
+        Node curr = new Node(root.val);
+        for(Node child : root.children) {
+            curr.children.add(cloneTree(child));
+        }
+        return curr;
+    }
+}
