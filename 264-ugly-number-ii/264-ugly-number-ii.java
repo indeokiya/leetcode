ef447627faa2ec722ab1ca72d0cca5db891116af
indeokiya <109320408+indeokiya@users.noreply.github.com>
2022-08-20 21:57:53 +0900

    Time: 1 ms (100.00%), Space: 42.1 MB (64.90%) - LeetHub

--- 264-ugly-number-ii/264-ugly-number-ii.java
+++ 264-ugly-number-ii/264-ugly-number-ii.java
@@ -1,27 +1,29 @@
-class Solution {
-    private int[] arr;
-    private int idx2=0, idx3=0, idx5=0;
-    
-    public void dp(int i) {
-        int multiplied2 = arr[idx2] * 2;
-        int multiplied3 = arr[idx3] * 3;
-        int multiplied5 = arr[idx5] * 5;
+class Ugly {
+    public int[] arr = new int[1690];
+    Ugly() {
+        arr[0] = 1;
+        int idx2=0, idx3=0, idx5=0;
         
-        arr[i] = Math.min(multiplied2, Math.min(multiplied3, multiplied5));
+        for (int i=1; i<1690; i++) {
+            int multiplied2 = arr[idx2] * 2;
+            int multiplied3 = arr[idx3] * 3;
+            int multiplied5 = arr[idx5] * 5;
         
-        if (arr[i] == multiplied2) idx2++;
-        if (arr[i] == multiplied3) idx3++;
-        if (arr[i] == multiplied5) idx5++;
+            arr[i] = Math.min(multiplied2, Math.min(multiplied3, multiplied5));
+        
+            if (arr[i] == multiplied2) idx2++;
+            if (arr[i] == multiplied3) idx3++;
+            if (arr[i] == multiplied5) idx5++;
+        }
     }
     
-    public int nthUglyNumber(int n) {
-        arr = new int[n];
-        arr[0] = 1;
-        for (int i=1; i<n; i++) {
-            dp(i);
-        }
+}
 
-        return arr[n-1];
+class Solution {
+    public static Ugly myUgly = new Ugly();
+    
+    public int nthUglyNumber(int n) {
+        return myUgly.arr[n-1];
     }
 }
 
