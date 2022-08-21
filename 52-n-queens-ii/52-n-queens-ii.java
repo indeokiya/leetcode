820aa0890a46d54dd599bc47d9794db8b1d9e76a
indeokiya <109320408+indeokiya@users.noreply.github.com>
2022-08-21 19:54:53 +0900

    Time: 4 ms (59.37%), Space: 39.8 MB (90.59%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.java
+++ 52-n-queens-ii/52-n-queens-ii.java
@@ -1,50 +1,50 @@
 class Solution {
-    private int[][] attacks;
+    private HashSet<Integer> cols, diags, antiDiags;
     private int count;
     private int n;
     
-    public boolean isPlaceable(int i, int j) {
-        return attacks[i][j] == 0;
-    }
-    
     public void checkRow(int i) {
         for (int j=0; j<n; j++) {
-            if (isPlaceable(i, j)) {
+            if (cols.contains(j) || diags.contains(i-j) || antiDiags.contains(i+j)) {
+                continue;
+            } else {
                 if (i==n-1) {
                     count++;
                 } else {
-                    placeQueen(i, j);    //when to delete?
-                    checkRow(i+1);                    
-                    removeQueen(i, j);
+                    cols.add(j); diags.add(i-j); antiDiags.add(i+j);
+                    checkRow(i+1);
+                    cols.remove(j); diags.remove(i-j); antiDiags.remove(i+j);
                 }
             }
         }
     }
     
-    public void placeQueen(int i, int j) {
-        for (int k=0; k<n; k++) {
-            attacks[k][j] += 1;
-            attacks[i][k] += 1;
-            if (i-j+k>=0 && i-j+k<n) attacks[i-j+k][k] += 1; 
-            if (i+j-k>=0 && i+j-k<n) attacks[i+j-k][k] += 1;
-        }
-        attacks[i][j] -= 3;
-    }
+//     public void placeQueen(int i, int j) {
+//         for (int k=0; k<n; k++) {
+//             attacks[k][j] += 1;
+//             attacks[i][k] += 1;
+//             if (i-j+k>=0 && i-j+k<n) attacks[i-j+k][k] += 1; 
+//             if (i+j-k>=0 && i+j-k<n) attacks[i+j-k][k] += 1;
+//         }
+//         attacks[i][j] -= 3;
+//     }
     
-    public void removeQueen(int i, int j) {
-        for (int k=0; k<n; k++) {
-            attacks[k][j] -= 1;
-            attacks[i][k] -= 1;
-            if (i-j+k>=0 && i-j+k<n) attacks[i-j+k][k] -= 1; 
-            if (i+j-k>=0 && i+j-k<n) attacks[i+j-k][k] -= 1;
-        }
-        attacks[i][j] += 3;        
-    }
+//     public void removeQueen(int i, int j) {
+//         for (int k=0; k<n; k++) {
+//             attacks[k][j] -= 1;
+//             attacks[i][k] -= 1;
+//             if (i-j+k>=0 && i-j+k<n) attacks[i-j+k][k] -= 1; 
+//             if (i+j-k>=0 && i+j-k<n) attacks[i+j-k][k] -= 1;
+//         }
+//         attacks[i][j] += 3;        
+//     }
     
     public int totalNQueens(int n) {
         count = 0;
         this.n = n;
-        attacks = new int[n][n];
+        cols = new HashSet<>();
+        diags = new HashSet<>();
+        antiDiags = new HashSet<>();
 
         checkRow(0);
 
