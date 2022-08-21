b74b440df189226e8c661e6b3f7d82e6b6a25be3
indeokiya <109320408+indeokiya@users.noreply.github.com>
2022-08-21 20:16:49 +0900

    Time: 7 ms (34.99%), Space: 41.9 MB (24.23%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.java
+++ 52-n-queens-ii/52-n-queens-ii.java
@@ -1,23 +1,33 @@
 class Solution {
-    public int checkRow(int i, Set<Integer> cols, Set<Integer> diags, Set<Integer> antiDiags, int n) {
-        if (i == n) return 1;
-        
-        int count = 0;
-        
+    private HashSet<Integer> cols, diags, antiDiags;
+    private int count;
+    private int n;
+    
+    public void checkRow(int i) {
         for (int j=0; j<n; j++) {
             if (cols.contains(j) || diags.contains(i-j) || antiDiags.contains(i+j)) {
                 continue;
-            } 
-        
-            cols.add(j); diags.add(i-j); antiDiags.add(i+j);
-            count += checkRow(i+1, cols, diags, antiDiags, n);
-            cols.remove(j); diags.remove(i-j); antiDiags.remove(i+j);
+            } else {
+                if (i==n-1) {
+                    count++;
+                } else {
+                    cols.add(j); diags.add(i-j); antiDiags.add(i+j);
+                    checkRow(i+1);
+                    cols.remove(j); diags.remove(i-j); antiDiags.remove(i+j);
+                }
+            }
         }
-        
-        return count;
     }
     
     public int totalNQueens(int n) {
-        return checkRow(0, new HashSet<>(), new HashSet<>(), new HashSet<>(), n);
+        count = 0;
+        this.n = n;
+        cols = new HashSet<>();
+        diags = new HashSet<>();
+        antiDiags = new HashSet<>();
+
+        checkRow(0);
+
+        return count; 
     }
 }
