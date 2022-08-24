d4c9c0f33362d82e49a98660d1341cf7cd82edbf
indeokiya <109320408+indeokiya@users.noreply.github.com>
2022-08-24 21:32:58 +0900

    Time: 3 ms (98.67%), Space: 44 MB (79.63%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,35 +1,38 @@
 class Solution {
-    public int doBinarySearch(List<Integer> list, int num) {
+    public int binarySearch(int[] res, int num, int size) {
         int left = 0;
-        int right = list.size()-1;
-        int mid = (left+right)/2;
+        int right = size;
+
         while (left<right) {
-            if (list.get(mid) < num) {
+            int mid = (left+right) >> 1;
+            if (res[mid] < num) {
                 left = mid+1;
-            } else if (list.get(mid) > num ) {
+            } else if (res[mid] > num ) {
                 right = mid;
             } else {
                 return mid;
             }
-            mid = (left+right)/2;
+            mid = (left+right) >> 1;
         }
         
         return right;
     }
     
     public int lengthOfLIS(int[] nums) {
-        List<Integer> myList = new LinkedList<>();
-        myList.add(nums[0]);
+        int[] res = new int[nums.length];
 
-        for (int i=1; i<nums.length; i++) {
-            if (nums[i] > myList.get(myList.size()-1)) {
-                myList.add(nums[i]);
-            } else {
-                int idx = doBinarySearch(myList, nums[i]);
-                myList.set(idx, nums[i]);
-            }
-        }
+		res[0] = nums[0];
+		int size = 0;
+		for (int i=1; i<nums.length; i++) {
+			int curr = nums[i];
+			if (curr > res[size]) {
+				res[++size] = curr;
+			} else {
+				int idx = binarySearch(res, curr, size);
+				res[idx] = curr;
+			}
+		}
         
-        return myList.size();
+        return size+1;
     }
 }
