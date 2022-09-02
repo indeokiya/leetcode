05176c3a5a4b098c5b07cccb1f574acd50e1a922
indeokiya <109320408+indeokiya@users.noreply.github.com>
2022-09-02 20:33:44 +0900

    Time: 0 ms, Memory: 39.9 MB - LeetHub

---
+++ spiral-matrix/spiral-matrix.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> ans = new ArrayList<>();
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        int upper_bound = 0, lower_bound = m-1, left_bound = 0, right_bound = n-1;
+        int num = 0;
+        
+        while (true) {
+            for (int j=left_bound; j<=right_bound; j++) {
+                ans.add(matrix[upper_bound][j]);
+                // System.out.println(ans.size());
+                if (++num == m*n) return ans;
+            }
+
+            for (int i=upper_bound+1; i<lower_bound; i++) {
+                ans.add(matrix[i][right_bound]);
+                                // System.out.println(ans.size());
+                if (++num == m*n) return ans;
+            }
+            
+            for (int j=right_bound; j>=left_bound; j--) {
+                ans.add(matrix[lower_bound][j]);
+                                // System.out.println(ans.size());
+                if (++num == m*n) return ans;   
+            }
+            
+            for (int i=lower_bound-1; i>upper_bound; i--) {
+                ans.add(matrix[i][left_bound]);
+                                // System.out.println(ans.size());
+                if (++num == m*n) return ans;   
+            }
+            upper_bound++;
+            right_bound--;
+            lower_bound--;
+            left_bound++;
+        }
+    }
+}
