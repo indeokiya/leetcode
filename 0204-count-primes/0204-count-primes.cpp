356fe366c16e2ec746f6dfa30604affb220732c2
indeokiya <109320408+indeokiya@users.noreply.github.com>
2023-03-09 20:38:18 +0900

    Time: 75 ms (99.84%), Space: 11.5 MB (18.05%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -2,20 +2,21 @@ class Solution {
     
 public:
     int countPrimes(int n) {
-        if (n==0) return 0;
+        if (n < 3) return 0;
         bool isPrime[n];
-        memset(isPrime, false, sizeof(isPrime));
-        int ret = 0;
+        memset(isPrime, true, sizeof(isPrime));
+        int cnt = n >> 1;
         
-        for (int i=2; i<n; i++) {
-            if (!isPrime[i]) {
-                ret++;
-                for (int j=i; j<n; j += i) {
-                    isPrime[j] = true;
+        for (int i=3; i*i<n; i += 2) {
+            if (isPrime[i]) {
+                for (int j=i*i; j<n; j += 2*i) {
+                    if (!isPrime[j]) continue;
+                    isPrime[j] = false;
+                    cnt--;
                 }
             }
         }
         
-        return ret;
+        return cnt;
     }
 };
