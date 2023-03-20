b694d5ef4dc7fe904e109554532a490437e57524
indeokiya <109320408+indeokiya@users.noreply.github.com>
2023-03-20 21:21:41 +0900

    Time: 4 ms (99.08%), Space: 8.3 MB (98.47%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,7 +1,4 @@
-struct Pos {
-    int r, c, dist, remaining;
-    Pos(int _r, int _c, int _dist, int _remaining) {r = _r; c = _c; dist = _dist; remaining = _remaining;}
-};
+typedef pair<pair<int, int>, int> pp;
 
 class Solution {
 private:
@@ -9,13 +6,12 @@ private:
     int N, M;
     int dr[4] = {0, 1, 0, -1};
     int dc[4] = {1, 0, -1, 0};
-    bool visited[40][40][80];
     
     bool isValid(int n, int m) {
         return n>=0 && n<N && m>=0 && m<M;
     }
     
-    queue<Pos> q;
+    queue<pp> q;
     
 public: 
     int shortestPath(vector<vector<int>>& grid, int k) {
@@ -24,35 +20,35 @@ public:
         cnt = N*M;
         if (k >= N+M-2) return N+M-2;
         
-        q.push(Pos(0, 0, 0, k));
+        vector<vector<int> > visited(N, vector<int>(M, INT_MAX));
+        visited[0][0] = 0;
+        
+        q.push({{0, 0}, 0});
         
         while (!q.empty()) {
-            Pos pos = q.front(); q.pop();
-            int r = pos.r;
-            int c = pos.c;
-            int dist = pos.dist;
-            int remaining = pos.remaining;
-            if (dist + N-1 + M-1  - r - c >= cnt) continue;
+            int r = q.front().first.first;
+            int c = q.front().first.second;
+            int cost = q.front().second;
+            q.pop();
+            
             if (r==N-1 && c==M-1) {
-                cnt = min(cnt, dist);
-                continue;
+                return cost;
             }
-            if (visited[r][c][remaining]) continue;
-            visited[r][c][remaining] = true;
             
             for (int i=0; i<4; i++) {
                 int nR = r + dr[i];
                 int nC = c + dc[i];
                 if (!isValid(nR, nC)) continue;
-                if (grid[nR][nC] && remaining > 0) {
-                    q.push(Pos(nR, nC, dist+1, remaining-1));
-                } else if (!grid[nR][nC]){
-                    q.push(Pos(nR, nC, dist+1, remaining));
+                if (visited[nR][nC] > visited[r][c] + grid[nR][nC]) {
+                    visited[nR][nC] = visited[r][c] + grid[nR][nC];
+                    if (visited[nR][nC] <= k) {
+                        q.push({{nR, nC}, cost+1});
+                    }
                 }
             }
         }
         
         
-        return cnt == N*M ? -1 : cnt;
+        return -1;
     }
 };
