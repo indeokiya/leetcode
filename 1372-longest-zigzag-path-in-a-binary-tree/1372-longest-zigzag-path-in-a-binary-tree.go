5db64926f53ff71946ac5e90acb426c9ec70253e
indeokiya <109320408+indeokiya@users.noreply.github.com>
2023-06-04 00:11:51 +0900

    Time: 115 ms (71.37%), Space: 16.1 MB (39.01%) - LeetHub

--- 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.go
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.go
@@ -15,23 +15,24 @@ func max(lhs, rhs int) int {
 }
 
 func longestZigZag(root *TreeNode) int {
-    length := new(int)
-    rec(root.Left, true, 1, length)
-    rec(root.Right, false, 1, length)
-    return *length
-}
+	length := new(int)
 
-func rec(root *TreeNode, fromLeft bool, depth int, length *int) {
-    if root == nil {
-        *length = max(*length, depth-1)
-        return
-    }
-    
-    if fromLeft == true {
-        rec(root.Right, false, 1 + depth, length)
-        rec(root.Left, true, 1, length)
-    } else {
-        rec(root.Left, true, 1 + depth, length)
-        rec(root.Right, false, 1, length)
-    }
+	var rec func(*TreeNode, bool, int, *int)
+	rec = func(root *TreeNode, fromLeft bool, depth int, length *int) {
+		if root == nil {
+			*length = max(*length, depth-1)
+			return
+		}
+
+		if fromLeft == true {
+			rec(root.Right, false, 1+depth, length)
+			rec(root.Left, true, 1, length)
+		} else {
+			rec(root.Left, true, 1+depth, length)
+			rec(root.Right, false, 1, length)
+		}
+	}
+
+	rec(root, true, 0, length)
+	return *length
 }
