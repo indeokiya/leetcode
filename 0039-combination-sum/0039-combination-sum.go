7492767eee9ae330d807935a7587cdd2430f5567
indeokiya <109320408+indeokiya@users.noreply.github.com>
2023-06-04 00:51:58 +0900

    Time: 4 ms (59.37%), Space: 2.8 MB (99.34%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.go
@@ -0,0 +1,29 @@
+func combinationSum(candidates []int, target int) [][]int {
+	answers := new([][]int)
+	currSlice := new([]int)
+
+	backTracking(0, &candidates, 0, target, answers, currSlice)
+
+	return *answers
+}
+
+func backTracking(index int, candidates *[]int, sum int, target int, answers *[][]int, currSlice *[]int) {
+	if index >= len(*candidates) || sum+2 > target {
+		return
+	}
+
+	backTracking(index+1, candidates, sum, target, answers, currSlice)
+
+	sum += (*candidates)[index]
+	*currSlice = append(*currSlice, (*candidates)[index])
+
+	if sum == target {
+		newSlice := make([]int, len(*currSlice))
+		copy(newSlice, *currSlice)
+		*answers = append(*answers, newSlice)
+	}
+
+
+	backTracking(index, candidates, sum, target, answers, currSlice)
+	*currSlice = (*currSlice)[:len(*currSlice)-1]
+}
