702d3c79200f83b3a62b299779f3bd8399db1897
indeokiya <109320408+indeokiya@users.noreply.github.com>
2023-06-12 19:03:29 +0900

    Time: 3 ms (88.89%), Space: 3.7 MB (100.00%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.go
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.go
@@ -1,53 +1,41 @@
 func knightProbability(n int, k int, row int, column int) float64 {
 	memo := make([][][]float64, k+1, k+1)
-	visited := make([][][]bool, k+1, k+1)
-    slices1 := make([]float64, n*n*(k+1), n*n*(k+1))
-    slices2 := make([]bool, n*n*(k+1), n*n*(k+1))
+    slices := make([]float64, n*n*(k+1), n*n*(k+1))
 
 	for i := 0; i < k+1; i++ {
-		square1 := make([][]float64, n, n)
-		square2 := make([][]bool, n, n)
+		square := make([][]float64, n, n)
 		for j := 0; j < n; j++ {
-			square1[j], slices1 = slices1[:n], slices1[n:]
-			square2[j], slices2 = slices2[:n], slices2[n:]
+			square[j], slices = slices[:n], slices[n:]
 		}
-		memo[i] = square1
-		visited[i] = square2
+		memo[i] = square
 	}
 
-	dr := []int{-2, -1, 1, 2, 2, 1, -1, -2}
+    memo[0][row][column] = 1.0
+    
+    dr := []int{-2, -1, 1, 2, 2, 1, -1, -2}
 	dc := []int{1, 2, 2, 1, -1, -2, -2, -1}
-
-    answer := float64(0)
+    
+    for depth:=1; depth<=k; depth++ {
+        for r:=0; r<n; r++ {
+            for c:=0; c<n; c++ {
+                for i:=0; i<8; i++ {
+                    nR, nC := r+dr[i], c+dc[i]
+                    if nR<0 || nR>=n || nC<0 || nC>=n {
+                        continue
+                    }
+                    memo[depth][r][c] += memo[depth-1][nR][nC] / 8.0
+                }            
+            }
+        }
+    }
+    
+    answer := 0.0
     
     for i:=0; i<n; i++ {
         for j:=0; j<n; j++ {
-            answer += dp(i, j, k, n, &memo, &visited, &dr, &dc, row, column) 
+            answer += memo[k][i][j]
         }
     }
 	return answer
 }
 
-func dp(r, c, depth, n int, memo *[][][]float64, visited *[][][]bool, dr, dc *[]int, R, C int) float64 {
-	if (*visited)[depth][r][c] {
-		return (*memo)[depth][r][c]
-	}
-
-	if depth == 0 {
-		if r == R && c == C {
-			return 1
-		}
-		return 0
-	}
-
-	for i := 0; i < 8; i++ {
-		nR, nC := r+(*dr)[i], c+(*dc)[i]
-		if nR < 0 || nR >= n || nC < 0 || nC >= n {
-			continue
-		}
-		(*memo)[depth][r][c] += dp(nR, nC, depth-1, n, memo, visited, dr, dc, R, C) / 8
-	}
-
-    (*visited)[depth][r][c] = true
-	return (*memo)[depth][r][c]
-}
