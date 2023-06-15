490d4d7d5ee04a98e70527ae0048978dfcb97e13
indeokiya <109320408+indeokiya@users.noreply.github.com>
2023-06-15 14:37:23 +0900

    Time: 51 ms (40.38%), Space: 24.8 MB (19.23%) - LeetHub

---
+++ 1230-toss-strange-coins/1230-toss-strange-coins.go
@@ -0,0 +1,20 @@
+func probabilityOfHeads(prob []float64, target int) float64 {
+    N := len(prob)
+    memo := make([][]float64, N, N)
+    slices := make([]float64, N*(N+1), N*(N+1))
+    for i:=0; i<N; i++ {
+        memo[i], slices = slices[:N+1], slices[N+1:]
+    }
+    
+    memo[0][0] = 1-prob[0]
+    memo[0][1] = prob[0]
+    for i:=1; i<N; i++ {
+        for j:=0; j<=i; j++ {
+            memo[i][j] += memo[i-1][j] * (1-prob[i])
+            memo[i][j+1] += memo[i-1][j] * prob[i]
+            
+        }
+    }
+    
+    return memo[N-1][target]
+}
