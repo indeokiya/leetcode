c397465d728a305e96ba51a299708c8aab465080
indeokiya <109320408+indeokiya@users.noreply.github.com>
2023-06-15 14:50:38 +0900

    Time: 8 ms (98.08%), Space: 3.8 MB (100.00%) - LeetHub

--- 1230-toss-strange-coins/1230-toss-strange-coins.go
+++ 1230-toss-strange-coins/1230-toss-strange-coins.go
@@ -1,22 +1,17 @@
 func probabilityOfHeads(prob []float64, target int) float64 {
     N := len(prob)
-    memo := make([][]float64, N, N)
-    slices := make([]float64, N*(N+1), N*(N+1))
-    for i:=0; i<N; i++ {
-        memo[i], slices = slices[:N+1], slices[N+1:]
-    }
+    memo := make([]float64, target+2, target+2)
     
-    memo[0][0] = 1-prob[0]
-    memo[0][1] = prob[0]
+    memo[0] = 1-prob[0]
+    memo[1] = prob[0]
     for i:=1; i<N; i++ {
-        for j:=0; j<=min(target,i); j++ {
-            memo[i][j] += memo[i-1][j] * (1-prob[i])
-            memo[i][j+1] += memo[i-1][j] * prob[i]
-            
+        for j:=min(target, i); j>=0; j-- {
+            memo[j+1] += memo[j] * prob[i]
+            memo[j] *= 1-prob[i]
         }
     }
     
-    return memo[N-1][target]
+    return memo[target]
 }
 
 func min(a, b int) int {
