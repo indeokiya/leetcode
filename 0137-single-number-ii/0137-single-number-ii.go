11054a71eaa59032f9b45c26a9d0813bac47735a
indeokiya <109320408+indeokiya@users.noreply.github.com>
2023-06-19 21:59:13 +0900

    Time: 3 ms (92.59%), Space: 3.4 MB (49.07%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.go
@@ -0,0 +1,10 @@
+func singleNumber(nums []int) int {
+    seenOnce, seenTwice := 0, 0
+    
+    for _, num := range nums {
+        seenOnce = seenOnce^num & ^seenTwice
+        seenTwice = seenTwice^num & seenOnce
+    }
+    
+    return seenOnce
+}
