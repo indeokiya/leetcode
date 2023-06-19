2a08aec6f63baa71ace37d6f91389469c860fea4
indeokiya <109320408+indeokiya@users.noreply.github.com>
2023-06-19 19:35:08 +0900

    Time: 6 ms (30.77%), Space: 2.5 MB (20.41%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.go
+++ 0190-reverse-bits/0190-reverse-bits.go
@@ -3,7 +3,7 @@ func reverseBits(num uint32) uint32 {
     num = num&0xcccccccc >> 2 | num&0x33333333 << 2
     num = num&0xf0f0f0f0 >> 4 | num&0x0f0f0f0f << 4
     num = num&0xff00ff00 >> 8 | num&0x00ff00ff << 8
-    num = num&0xffff0000 >> 16 | num&0x0000ffff << 16
+    num = num>>16 | num<<16
     
     return num
 }
