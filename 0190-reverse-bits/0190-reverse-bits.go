d72e4a65aee5b2f9a87e8cb0aea6615d807b3878
indeokiya <109320408+indeokiya@users.noreply.github.com>
2023-06-19 19:33:48 +0900

    Time: 3 ms (55.33%), Space: 2.5 MB (97.34%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.go
+++ 0190-reverse-bits/0190-reverse-bits.go
@@ -1,17 +1,9 @@
 func reverseBits(num uint32) uint32 {
-    answer := uint32(0)
-    mask1, mask2 := uint32(0x1<<31), uint32(0x1)
+    num = num&0xaaaaaaaa >> 1 | num&0x55555555 << 1
+    num = num&0xcccccccc >> 2 | num&0x33333333 << 2
+    num = num&0xf0f0f0f0 >> 4 | num&0x0f0f0f0f << 4
+    num = num&0xff00ff00 >> 8 | num&0x00ff00ff << 8
+    num = num&0xffff0000 >> 16 | num&0x0000ffff << 16
     
-    for i:=0; i<32; i++ {
-        if mask2 & num > 0 {
-            answer |= mask1    
-        }
-        
-        mask1 >>= 1
-        mask2 <<= 1
-    }
-    
-    
-    
-    return answer
+    return num
 }
