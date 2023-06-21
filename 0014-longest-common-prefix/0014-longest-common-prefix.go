a9d0ae85756c8d1796256c051919ae714fe88ece
indeokiya <109320408+indeokiya@users.noreply.github.com>
2023-06-21 16:26:17 +0900

    Time: 0 ms (100.00%), Space: 2.3 MB (37.55%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.go
@@ -0,0 +1,18 @@
+func longestCommonPrefix(strs []string) string {
+    ans := 0
+    
+    for i:=0; i<len(strs[0]); i++ {
+        for j:=1; j<len(strs); j++ {
+            if i == len(strs[j]) || strs[0][i] != strs[j][i] {
+                return strs[0][:ans]
+            }
+        }
+        ans++
+    }
+
+    
+    if ans == 0 {
+        return ""
+    }
+    return strs[0][:ans]
+}
