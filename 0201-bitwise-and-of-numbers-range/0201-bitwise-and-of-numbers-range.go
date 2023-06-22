e3862da095427d51828341f8f11f04033e8f4898
indeokiya <109320408+indeokiya@users.noreply.github.com>
2023-06-22 22:37:52 +0900

    Time: 20 ms (43.24%), Space: 4.8 MB (16.22%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.go
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.go
@@ -1,28 +1,7 @@
 func rangeBitwiseAnd(left int, right int) int {
-    
-    ans := 0
-    m, n := getMsb(left), getMsb(right)
-    
-    if m < n {
-        return ans
+    for left < right {
+        right &= right-1
     }
-    
-    for ; m>=1; m>>=1 {
-        if left & m != right & m {
-            break
-        }
-        ans |= left & m
-    }
-    
-    return ans
+    return left & right
 }
 
-func getMsb(n int) int {
-    n |= n>>1
-    n |= n>>2
-    n |= n>>4
-    n |= n>>8
-    n |= n>>16
-    n >>= 1
-    return n+1
-}
