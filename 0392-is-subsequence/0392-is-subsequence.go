bca9e2a9f53d7d40c8fcceda7a1de2e0be63a3ac
indeokiya <109320408+indeokiya@users.noreply.github.com>
2023-06-26 16:35:17 +0900

    Time: 1 ms (68.52%), Space: 2 MB (98.89%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.go
@@ -0,0 +1,17 @@
+func isSubsequence(s string, t string) bool {
+    M, N := len(s), len(t)
+    if M > N {
+        return false
+    }
+    
+    p1, p2 := 0, 0
+    
+    for p1<M && p2<N {
+        if s[p1] == t[p2] {
+            p1++
+        }
+        p2++
+    }
+    
+    return p1==M
+}
