3722df4d389cdaf45e02a84c335571f98894c20a
indeokiya <109320408+indeokiya@users.noreply.github.com>
2023-06-27 02:32:29 +0900

    Time: 8 ms (35.71%), Space: 5.5 MB (76.59%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.go
+++ 0072-edit-distance/0072-edit-distance.go
@@ -4,7 +4,7 @@ func minDistance(word1 string, word2 string) int {
     memo := make([][]int, M+1, M+1)
     slices := make([]int, (M+1)*(N+1), (M+1)*(N+1))
     for i:=0; i<=M; i++ {    
-        memo[i] = slices[i*(N+1):(i+1)*(N+1)]
+        memo[i], slices = slices[:N+1], slices[N+1:]
     }
     
     for j:=1; j<=N; j++ {
