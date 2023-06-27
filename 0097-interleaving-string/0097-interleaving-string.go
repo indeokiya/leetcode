76f0be2f78e72fee3204cb56f5ce5b098e4d48b1
indeokiya <109320408+indeokiya@users.noreply.github.com>
2023-06-27 19:15:45 +0900

    Time: 2 ms (77.98%), Space: 2 MB (99.08%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.go
+++ 0097-interleaving-string/0097-interleaving-string.go
@@ -4,34 +4,28 @@ func isInterleave(s1 string, s2 string, s3 string) bool {
         return false
     }
     
-    memo := make([][]bool, M+1, M+1)
-    slices := make([]bool, (M+1)*(N+1))
-    for i:=0; i<=M; i++ {
-        memo[i], slices = slices[:N+1], slices[N+1:]
+    if M < N {
+        return isInterleave(s2, s1, s3)
     }
+    
+    memo := make([]bool, N+1, N+1)
                    
-    memo[0][0] = true
+    memo[0] = true
     for j:=1; j<=N; j++ {
         if s2[j-1] != s3[j-1] {
             break
         }
-        memo[0][j] = true
+        memo[j] = true
     }
     
     for i:=1; i<=M; i++ {
-        if s1[i-1] != s3[i-1] {
-            break
-        }
-        memo[i][0] = true
-    }
-    
-    for i:=1; i<=M; i++ {
+        memo[0] = memo[0] && s3[i-1] == s1[i-1]
         for j:=1; j<=N; j++ {
-            memo[i][j] = (memo[i-1][j] && s3[i+j-1] == s1[i-1]) || (memo[i][j-1] && s3[i+j-1] == s2[j-1])
+            memo[j] = (memo[j] && s3[i+j-1] == s1[i-1]) || (memo[j-1] && s3[i+j-1] == s2[j-1])
         }
     }
     
-    return memo[M][N]
+    return memo[N]
 }
 
 /*
