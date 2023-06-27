87408d245e46904edf9c772c511502b1cc5da66b
indeokiya <109320408+indeokiya@users.noreply.github.com>
2023-06-27 20:42:46 +0900

    Time: 121 ms (50.21%), Space: 10 MB (77.57%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.go
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.go
@@ -1,15 +1,10 @@
 func containsNearbyDuplicate(nums []int, k int) bool {
-    seen := map[int]int{}
+    seen := make(map[int]int, len(nums))
     
     for i:=0; i<len(nums); i++ {
-        index, ok := seen[nums[i]]
-        if !ok {
-            seen[nums[i]] = i
-            continue
-        }
-        if i - index <= k {
+        if index, ok := seen[nums[i]]; ok && i - index <= k {
             return true
-        } 
+        }
         seen[nums[i]] = i
     }
     
