430dc0c2592ecebf7aee5e3717148767e1323d1e
indeokiya <109320408+indeokiya@users.noreply.github.com>
2023-07-05 12:32:19 +0900

    Time: 71 ms (82.03%), Space: 45.2 MB (63.43%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,39 +1,32 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Set<String> set = new HashSet<>();
         Set<String> words = new HashSet<>(wordList);
         if (!words.contains(endWord)) {
             return 0;
         }
         Queue<String> q = new LinkedList<>();
-        set.add(beginWord);
         q.add(beginWord);
+        
         for (int step=1; !q.isEmpty(); step++) {
             int size = q.size();
             while (size-->0) {
-                String from = q.poll();
-                for (String to : wordList) {
-                    if (able(from, to) && !set.contains(to)) {
-                        if (to.equals(endWord)) {
-                            return step + 1;
+                char[] from = q.poll().toCharArray();
+                for (int i=0; i<from.length; i++) {
+                    char original = from[i];
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        from[i] = c;
+                        String changed = new String(from);
+                        if (words.contains(changed)) {
+                            if (changed.equals(endWord)) return step + 1;
+                            q.add(changed);
+                            words.remove(changed);
                         }
-                        set.add(to);
-                        q.add(to);
                     }
+                    from[i] = original;
                 }
             }
         }
         
         return 0;
-    }
-    
-    public boolean able(String a, String b) {
-        int diff = 0;
-        for (int i=0; i<a.length(); i++) {
-            if (a.charAt(i) != b.charAt(i)) {
-                diff++;
-            }
-        }
-        return diff==1;
     }
 }
