a4c9de0751a1c9f3103cc8162b08366085254163
indeokiya <109320408+indeokiya@users.noreply.github.com>
2023-07-05 12:24:58 +0900

    Time: 1761 ms (6.65%), Space: 44.3 MB (83.78%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> set = new HashSet<>();
+        Queue<String> q = new LinkedList<>();
+        set.add(beginWord);
+        q.add(beginWord);
+        for (int step=1; !q.isEmpty(); step++) {
+            int size = q.size();
+            while (size-->0) {
+                String from = q.poll();
+                for (String to : wordList) {
+                    if (able(from, to) && !set.contains(to)) {
+                        if (to.equals(endWord)) {
+                            return step + 1;
+                        }
+                        set.add(to);
+                        q.add(to);
+                    }
+                }
+            }
+        }
+        
+        return 0;
+    }
+    
+    public boolean able(String a, String b) {
+        int diff = 0;
+        for (int i=0; i<a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i)) {
+                diff++;
+            }
+        }
+        return diff==1;
+    }
+}
