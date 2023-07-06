107323d91e13bd2ce0b89e594c5b9d4eb0159d3a
indeokiya <109320408+indeokiya@users.noreply.github.com>
2023-07-07 03:26:21 +0900

    Time: 1331 ms (28%), Space: 0B (100%) - LeetHub

---
+++ 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+select employee_id, department_id
+from Employee
+where primary_flag = 'Y'
+union
+select employee_id, department_id
+from Employee
+where employee_id not in (
+    select employee_id
+    from Employee
+    where primary_flag = 'Y'
+)
