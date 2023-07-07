5abd2095a4bff7599a3618a2143dd79bc88935e1
indeokiya <109320408+indeokiya@users.noreply.github.com>
2023-07-07 16:13:40 +0900

    Time: 2048 ms (62.81%), Space: 0B (100%) - LeetHub

---
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+
+SELECT 
+    name, bonus
+FROM 
+    Employee e
+LEFT JOIN 
+    Bonus b
+ON 
+    e.empId = b.empId
+WHERE
+    bonus IS NULL OR
+    bonus < 1000
