7e218fe34c5429221e6c7ade160ed92195619ee5
indeokiya <109320408+indeokiya@users.noreply.github.com>
2023-08-04 22:07:42 +0900

    Time: 4 ms (71.59%), Space: 43.5 MB (85.23%) - LeetHub

--- 2065-check-for-contradictions-in-equations/2065-check-for-contradictions-in-equations.java
+++ 2065-check-for-contradictions-in-equations/2065-check-for-contradictions-in-equations.java
@@ -1,59 +1,66 @@
 class Solution {
-    private Map<String, Integer> map;
-    private int[] uf;
-    private double[] weight;
     private final double E = 1e-5;
 
     public boolean checkContradictions(List<List<String>> equations, double[] values) {        
-        map = new HashMap<>();
-        uf = IntStream.range(0, 200).toArray();
-        weight = new double[200];
-        Arrays.fill(weight, 1);
+        final int N = values.length;
+        Map<String, Integer> map = new HashMap<>();
+        UF uf = new UF(N);
     
-        for (int i=0, idx=0; i<values.length; i++) {
+        for (int i=0; i<N; i++) {
             String a = equations.get(i).get(0);
             String b = equations.get(i).get(1);
-            if (!map.containsKey(a)) {
-                map.put(a, idx++);
-            }
-            if (!map.containsKey(b)) {
-                map.put(b, idx++);
-            }
+            map.putIfAbsent(a, map.size());
+            map.putIfAbsent(b, map.size());
             int num1 = map.get(a);
             int num2 = map.get(b);
-            int root1 = find(num1);
-            int root2 = find(num2);
-            System.out.println("num1: " + num1 + ", root1: " + root1 + ", num2: " + num2 + ", root2: " + root2);
-            System.out.println("weight " + a + ": " + weight[num1] + ", weight " + b + ": " + weight[num2]);
+            int root1 = uf.find(num1);
+            int root2 = uf.find(num2);
+        
             if (root1 == root2) {
-                if (Math.abs(weight[num2]/weight[num1] - values[i]) >= E) {
+                if (uf.merge(num1, num2, values[i])) {
                     return true;
                 }
             } else {
-                merge(num1, num2, values[i]);
+                uf.merge(num1, num2, values[i]);
             }
-            System.out.println("weight 1: " + weight[num1] + ", weight 2: " + weight[num2]);
         }
 
         return false;
     }
 
-    private int find(int n) {
-        if (n == uf[n]) {
-            return n;
-        }
-        int root = find(uf[n]);
-        weight[n] *= weight[uf[n]];
-        uf[n] = root;
-        return uf[n];
-    }
+    private class UF {
+        int[] uf;
+        double[] weight;
 
-    private void merge(int a, int b, double val) {
-        if (find(b) != b) {
-            weight[find(b)] = 1/weight[b];
-            uf[find(b)] = b;
+        UF(int n) {
+            uf = IntStream.range(0, 2*n).toArray();
+            weight = new double[2*n];
+            Arrays.fill(weight, 1);
         }
-        uf[b] = a;
-        weight[b] = val;
+
+        private int find(int n) {
+            if (n == uf[n]) {
+                return n;
+            }
+            int root = find(uf[n]);
+            weight[n] *= weight[uf[n]];
+            uf[n] = root;
+            return uf[n];
+        }
+
+        private boolean merge(int a, int b, double val) {
+            int rootA = uf[a];
+            int rootB = uf[b];
+            if (rootA == rootB) {
+                return Math.abs(weight[b]/weight[a] - val) >= E;
+            }
+            if (rootB != b) {
+                weight[rootB] = 1/weight[b];
+                uf[rootB] = b;
+            }
+            uf[b] = a;
+            weight[b] = val;
+            return false;
+        }
     }
 }
