65cfa39e59a6b019117285f187e8975b78d1bd74
indeokiya <109320408+indeokiya@users.noreply.github.com>
2023-08-04 17:56:57 +0900

    Time: 731 ms (71.72%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,12 +1,8 @@
 # Write your MySQL query statement below
 
 SELECT
-    IFNULL(CASE
-        WHEN id%2 = 1 THEN LEAD(id,1) OVER()
-        ELSE LAG(id,1) OVER()
-    END, id) as id,
+    ROW_NUMBER() OVER() AS id,
     student
 FROM
     Seat
-ORDER BY
-    id
+ORDER BY IF(MOD(id, 2) = 0, id-1, id+1)
