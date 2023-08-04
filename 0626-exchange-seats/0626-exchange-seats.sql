0777fed64942bb23ccd4637d5f75bb0e2d924452
indeokiya <109320408+indeokiya@users.noreply.github.com>
2023-08-04 17:55:14 +0900

    Time: 868 ms (35.46%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,13 +1,10 @@
 # Write your MySQL query statement below
 
 SELECT
-    (CASE
-        WHEN id%2 = 1 AND id < (SELECT COUNT(*) FROM Seat)
-        THEN id+1
-        WHEN id%2 = 0
-        THEN id-1
-        ELSE id
-    END) as id,
+    IFNULL(CASE
+        WHEN id%2 = 1 THEN LEAD(id,1) OVER()
+        ELSE LAG(id,1) OVER()
+    END, id) as id,
     student
 FROM
     Seat
