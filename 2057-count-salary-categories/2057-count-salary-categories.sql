c4d2db28e2b569a8d15ab7f84616888eb64d649e
indeokiya <109320408+indeokiya@users.noreply.github.com>
2023-08-04 17:00:24 +0900

    Time: 2878 ms (26.73%), Space: 0B (100%) - LeetHub

--- 2057-count-salary-categories/2057-count-salary-categories.sql
+++ 2057-count-salary-categories/2057-count-salary-categories.sql
@@ -2,18 +2,18 @@
 
 SELECT
     'Low Salary' AS category,
-    SUM(IF(income<20000, 1, 0)) AS accounts_count
+    (SELECT COUNT(*) FROM Accounts WHERE income < 20000) AS accounts_count
 FROM
     Accounts
 UNION
 SELECT
     'Average Salary' AS category,
-    SUM(IF(income>=20000 AND income<=50000, 1, 0)) AS accounts_count
+    (SELECT COUNT(*) FROM Accounts WHERE income >= 20000 AND income <= 50000) AS accounts_count
 FROM
     Accounts
 UNION
 SELECT
     'High Salary' AS category,
-    SUM(IF(income>50000, 1, 0)) AS accounts_count
+    (SELECT COUNT(*) FROM Accounts WHERE income > 50000) AS accounts_count
 FROM
     Accounts
