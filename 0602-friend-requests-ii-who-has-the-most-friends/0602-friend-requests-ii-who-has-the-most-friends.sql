6fc817a6a20e0a44385b9993aaf9d2c377110506
indeokiya <109320408+indeokiya@users.noreply.github.com>
2023-08-07 16:05:03 +0900

    Time: 876 ms (8.48%), Space: 0B (100%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,25 +1,18 @@
 # Write your MySQL query statement below
 
 WITH CTE AS (
-SELECT
-    requester_id,
-    accepter_id
-FROM
-    RequestAccepted
-UNION ALL
-SELECT
-    accepter_id AS requester_id,
-    requester_id AS accepter_id
-FROM
-    RequestAccepted
+    SELECT requester_id AS id
+    FROM RequestAccepted
+    UNION ALL
+    SELECT accepter_id AS id
+    FROM RequestAccepted
+), CTE2 AS (
+    SELECT id, COUNT(id) AS cnt, DENSE_RANK() OVER(ORDER BY COUNT(id) DESC) AS rn
+    FROM CTE
+    GROUP BY 1
 )
 
-SELECT
-    requester_id AS id,
-    COUNT(1) AS num
-FROM
-    CTE
-GROUP BY
-    requester_id
-HAVING
-    COUNT(1) = (SELECT COUNT(1) FROM CTE GROUP BY requester_id ORDER BY COUNT(1) DESC LIMIT 1)
+SELECT id, cnt AS num
+FROM CTE2
+WHERE rn = 1
+
