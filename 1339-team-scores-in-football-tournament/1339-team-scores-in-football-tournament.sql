2a5befe27b3e9eac0012a9f6b2ab84d1aa956b69
indeokiya <109320408+indeokiya@users.noreply.github.com>
2023-08-08 17:21:24 +0900

    Time: 1711 ms (63.91%), Space: 0B (100%) - LeetHub

---
+++ 1339-team-scores-in-football-tournament/1339-team-scores-in-football-tournament.sql
@@ -0,0 +1,28 @@
+# Write your MySQL query statement below
+
+WITH Host AS (
+    SELECT t.team_id, t.team_name, SUM(CASE
+        WHEN ISNULL(m.host_goals) THEN 0
+        WHEN m.host_goals > guest_goals THEN 3 
+        WHEN m.host_goals < guest_goals THEN 0
+        ELSE 1
+        END
+    ) AS num_points
+    FROM Teams t LEFT JOIN Matches m ON t.team_id = m.host_team
+    GROUP BY team_id
+), Guest AS (
+    SELECT t.team_id, t.team_name, SUM(CASE
+        WHEN ISNULL(m.host_goals) THEN 0
+        WHEN m.host_goals > guest_goals THEN 0 
+        WHEN m.host_goals < guest_goals THEN 3
+        ELSE 1
+        END
+    ) AS num_points
+    FROM Teams t LEFT JOIN Matches m ON t.team_id = m.guest_team
+    GROUP BY team_id    
+)
+
+SELECT team_id, team_name, SUM(num_points) AS num_points
+FROM (SELECT * FROM Host UNION ALL SELECT * FROM Guest) tt
+GROUP BY team_id
+ORDER BY num_points DESC, team_id ASC
