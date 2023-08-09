d226608a34c154689b2c0337e4eb58e9c2fb3607
indeokiya <109320408+indeokiya@users.noreply.github.com>
2023-08-09 16:02:29 +0900

    Time: 2409 ms (20.07%), Space: 0B (100%) - LeetHub

--- 1654-customer-order-frequency/1654-customer-order-frequency.sql
+++ 1654-customer-order-frequency/1654-customer-order-frequency.sql
@@ -1,12 +1,9 @@
 # Write your MySQL query statement below
 
 SELECT customer_id, name
-FROM (
-  SELECT *
-  FROM Orders o INNER JOIN Product p USING (product_id) INNER JOIN Customers c USING (customer_id)
-  WHERE YEAR(order_date) = 2020 AND MONTH(order_date) BETWEEN 6 AND 7
-  GROUP BY customer_id, MONTH(order_date)
-  HAVING SUM(quantity * price) >= 100
-) t
+FROM Orders o INNER JOIN Product p USING (product_id) INNER JOIN Customers c USING (customer_id)
+WHERE YEAR(order_date) = 2020 AND MONTH(order_date) BETWEEN 6 AND 7
 GROUP BY customer_id
-HAVING COUNT(1) > 1
+HAVING SUM((MONTH(order_date) = 6) * quantity * price) >= 100 AND
+        SUM((MONTH(order_date) = 7) * quantity * price) >= 100
+
