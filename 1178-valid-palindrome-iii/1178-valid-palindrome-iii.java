f3a7a9e7582b3763fdd1a56ca1c22cf9ab5ecae3
indeokiya <109320408+indeokiya@users.noreply.github.com>
2023-08-24 19:24:34 +0900

    Time: 11 ms (76.88%), Space: 40.4 MB (99.5%) - LeetHub

--- 1178-valid-palindrome-iii/1178-valid-palindrome-iii.java
+++ 1178-valid-palindrome-iii/1178-valid-palindrome-iii.java
@@ -1,18 +1,22 @@
 class Solution {
-    private int[][] memo;
+    private int[] memo;
 
     public boolean isValidPalindrome(String s, int k) {
-        memo = new int[s.length()][s.length()];
+        memo = new int[s.length()];
 
         for (int i=s.length()-2; i>=0; i--) {
+            int prev = 0;
             for (int j=i+1; j<s.length(); j++) {
-                memo[i][j] = s.charAt(i) == s.charAt(j)
-                    ? memo[i+1][j-1]
-                    : 1 + Math.min(memo[i+1][j], memo[i][j-1]);
+                int tmp = memo[j];
+                memo[j] = s.charAt(i) == s.charAt(j)
+                    ? prev
+                    : 1 + Math.min(memo[j], memo[j-1]);
+
+                prev = tmp;
             }
         }
 
-        return memo[0][s.length()-1] <= k;
+        return memo[s.length()-1] <= k;
     }
 }
 
