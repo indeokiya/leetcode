aa5bdf869f88c6935eb700aa61933e48c0511750
indeokiya <109320408+indeokiya@users.noreply.github.com>
2023-08-24 19:15:01 +0900

    Time: 13 ms (65.33%), Space: 44.6 MB (91.96%) - LeetHub

--- 1178-valid-palindrome-iii/1178-valid-palindrome-iii.java
+++ 1178-valid-palindrome-iii/1178-valid-palindrome-iii.java
@@ -1,33 +1,28 @@
 class Solution {
-    private int[][] count;
-    private String s;
+    private int[][] memo;
 
     public boolean isValidPalindrome(String s, int k) {
-        this.s = s;
-        count = new int[s.length()+1][s.length()+1];
-        for (int i=0; i<=s.length(); i++) {
-            Arrays.fill(count[i], -1);
-        }
+        memo = new int[s.length()][s.length()];
 
-        return helper(0, s.length()-1, k);
-    }
-
-    public boolean helper(int l, int r, int k) {
-        if (count[l][r] >= k) {
-            return false;
-        }
-        count[l][r] = k;
-
-        while (l<r) {
-            if (s.charAt(l) == s.charAt(r)) {
-                l++;
-                r--;
-                continue;
+        for (int i=s.length()-2; i>=0; i--) {
+            for (int j=i+1; j<s.length(); j++) {
+                memo[i][j] = s.charAt(i) == s.charAt(j)
+                    ? memo[i+1][j-1]
+                    : 1 + Math.min(memo[i+1][j], memo[i][j-1]);
             }
-
-            if (k==0) return false;
-            return helper(l, r-1, k-1) || helper(l+1, r, k-1);
         }
-        return true;
+
+        return memo[0][s.length()-1] <= k;
     }
 }
+
+/*
+
+0   ?   ?   ?
+    0   ?   ?
+        0   ?
+            0
+
+
+*/
+
