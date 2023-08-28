95a1ea13d2170f949a2618d6e4dd42d874864685
indeokiya <109320408+indeokiya@users.noreply.github.com>
2023-08-28 17:38:42 +0900

    Time: 1 ms (60.93%), Space: 38.8 MB (97.2%) - LeetHub

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -1,41 +1,14 @@
 class Solution {
     public int integerBreak(int n) {
-        if (n <= 3) return n-1;
-        if (n == 4) return 4;
+        int[] dp = new int[n+1];
 
-        int ans = 1;
-        while (n > 0) {
-            if (n == 4) {
-                ans *= 4;
-                break;
+        for (int i=1; i<n; i++) {
+            for (int j=1; j<=i; j++) {
+                if (i+j > n) continue;
+                dp[i+j] = Math.max(dp[i+j], Math.max(dp[i], i) * Math.max(dp[j], j));
             }
-            if (n >= 3) {
-                ans *= 3;
-                n -= 3;
-            } else {
-                ans *= 2;
-                n -= 2;
-            }
-
         }
 
-        return ans;
+        return dp[n];
     }
 }
-
-/*
-
-dp[i] = solution
-
-dp[2] = 1
-dp[3] = 2
-dp[4] = 4
-dp[5] = 2*3
-dp[6] = 3*3
-dp[7] = 3*4
-dp[8] = 2222 233   16 18 
-dp[9] = 333 
-
-dp[n] = max (i for range n-2...3 | dp[i] * n-i)
-
-*/
