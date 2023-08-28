a897a6172fe02bf8cf90eccc689b9a59c48c8344
indeokiya <109320408+indeokiya@users.noreply.github.com>
2023-08-28 23:11:01 +0900

    Time: 15 ms (100%), Space: 43.9 MB (99%) - LeetHub

--- 2325-number-of-ways-to-select-buildings/2325-number-of-ways-to-select-buildings.java
+++ 2325-number-of-ways-to-select-buildings/2325-number-of-ways-to-select-buildings.java
@@ -1,41 +1,25 @@
 class Solution {
     public long numberOfWays(String s) {
-        List<Long> list = new LinkedList<>();
-        
-        char prev = '-';
-        long count = 0;
+        int n0 = 0;
+        int n1 = 0;
+        long n01 = 0;
+        long n10 = 0;
+        long ans = 0;
 
         for (char c : s.toCharArray()) {
-            if (c != prev) {
-                prev = c;
-                list.add(count);
-                count = 1;
+            if (c == '0') {
+                ans += n01;
+                n0++;
+                n10 += n1;
                 continue;
             }
-            count++;
+
+            ans += n10;
+            n1++;
+            n01 += n0;
         }
 
-        list.remove(0);
-        list.add(count);
-
-        if (list.size() <= 2) return 0;
-
-        Long[] arr = list.toArray(new Long[0]);
-        long[] dp = new long[arr.length];
-
-        dp[2] = arr[0] * arr[1] * arr[2];
-        long[] n01s = new long[]{arr[0]*arr[1], 0};
-        long[] sums = new long[]{arr[0], arr[1]};
-        int curr = 0;
-
-        for (int i=2; i<arr.length; i++) {
-            dp[i] = dp[i-1] + arr[i] * n01s[curr];
-            curr ^= 1;
-            n01s[curr] += sums[curr] * arr[i];
-            sums[curr ^ 1] += arr[i];
-        }
-
-        return dp[arr.length-1];
+        return ans;
     }
 }
 
