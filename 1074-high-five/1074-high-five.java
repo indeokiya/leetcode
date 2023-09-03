b1968d36e8ae2909195ed5316d4c52d3bdb11a20
indeokiya <109320408+indeokiya@users.noreply.github.com>
2023-09-03 21:41:42 +0900

    Time: 14 ms (5.62%), Space: 44.5 MB (7.4%) - LeetHub

--- 1074-high-five/1074-high-five.java
+++ 1074-high-five/1074-high-five.java
@@ -1,12 +1,11 @@
 class Solution {
     public int[][] highFive(int[][] items) {
-        Map<Integer, List<Integer>> map = new HashMap<>();
+        Map<Integer, List<Integer>> map = new TreeMap<>();
         for (int[] item : items) {
             int id = item[0];
             int score = item[1];
             map.computeIfAbsent(id, k -> new LinkedList<>()).add(score);
         }
-
 
         List<int[]> answer = new ArrayList<>();
         for (int id : map.keySet()) {
@@ -18,7 +17,6 @@ class Solution {
                 .sum();
             answer.add(new int[]{id, (int)(sum/5)});
         }
-        Collections.sort(answer, (o1, o2) -> o1[0] - o2[0]);
 
         int[][] solArr = new int[map.size()][];
         return answer.toArray(solArr);
