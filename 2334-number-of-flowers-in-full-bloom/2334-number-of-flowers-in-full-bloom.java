a6299819c90a77ace5100c1969b09374cf724249
indeokiya <109320408+indeokiya@users.noreply.github.com>
2023-10-11 23:51:20 +0900

    Time: 64 ms (63.08%), Space: 75.5 MB (13.85%) - LeetHub

---
+++ 2334-number-of-flowers-in-full-bloom/2334-number-of-flowers-in-full-bloom.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] fullBloomFlowers(int[][] flowers, int[] people) {
+        int[] sortedPeople = Arrays.copyOf(people, people.length);
+        Arrays.sort(sortedPeople);
+
+        Arrays.sort(flowers, (a, b) -> Arrays.compare(a, b));
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        Map<Integer, Integer> dic = new HashMap<>();
+
+        int i = 0;
+        int iter = 0;
+        for (int person : sortedPeople) {
+            while (i < flowers.length && flowers[i][0] <= person) {
+                heap.offer(flowers[i][1]);
+                i++;
+            }
+
+            while (!heap.isEmpty() && heap.peek() < person) {
+                heap.poll();
+            }
+
+            dic.put(person, heap.size());
+        }
+
+        int[] answer = new int[people.length];
+        for (int j=0; j<people.length; j++) {
+            answer[j] = dic.get(people[j]);
+        }
+
+        return answer;
+    }
+}
